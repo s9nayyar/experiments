e1873879e084cc8d3432b377ebf556b44d5e3820
s9nayyar <s9nayyar@gmail.com>
2019-10-04 10:14:55 -0300

    This is a Test class.

---
+++ TestJava/src/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author sumit.nayyar
+ * This is a test class.
+ */
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test class !!");
+	}
+
+}
